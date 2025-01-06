61deb90ac9ed85300680c2b06b3a2ba70d8d5621
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-01-06 16:39:04 +0100

    append user card to form page

--- js/form.js
+++ js/form.js
@@ -2,12 +2,32 @@ const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
+  // Get input values
+  const userQuestion = document.querySelector(
+    '[data-js="user-question"]'
+  ).value;
+  const userAnswer = document.querySelector('[data-js="user-answer"]').value;
+  const userTag = document.getElementById("user-tag").value;
 
-  console.log("data", data);
-  event.target.reset();
-  event.target.elements.firstName.focus();
+  // Create card element
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  // Create elements for question, answer, and tag
+  const newQuestion = document.createElement("h2");
+  newQuestion.textContent = userQuestion;
+
+  const newAnswer = document.createElement("h3");
+  newAnswer.textContent = userAnswer;
+
+  const newTag = document.createElement("span");
+  newTag.classList.add("userTag");
+  newTag.textContent = userTag;
+
+  // Append elements to the card
+  card.append(newQuestion, newAnswer, newTag);
+
+  form.insertAdjacentElement("afterend", card);
 });
 
 const userQuestion = document.querySelector('[data-js="user-question"]');
@@ -40,10 +60,4 @@ userAnswer.addEventListener("input", (event) => {
 
   console.log("first approach: ", event.target.value);
   console.log("second approach: ", userAnswer.value);
-  // If you're only using the input element once an it's clear in
-  // the context of your code, you might prefer the second approach
-  // (input.value). if you're dealing with multiple input elements
-  // or if you wnt to reuse the same event handler function for
-  // different input elements, the first approach (event.target.value)
-  // might be more versatile.
 });
